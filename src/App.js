1c68ee3a2f6c4e884dd0004c6d43ef178c542beb
ABHISHEK VERMA <104015898+AbhiishDev@users.noreply.github.com>
2022-11-22 23:47:50 +0530

    changed more things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import React from'react';
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-
-} from "react-router-dom";
-
-import Login from "./components/Login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Routes>
-          <Route exact path="/" element ={ <Login />}/>
-    
+        <Routes>
+          <Route exact path="/" element={ <Login />}/>
+           
+        
+          <Route  path="/home" element={ <Header />} />
+          <Route  path="/home" element={    <Home />}/>
+         
+
         </Routes>
       </Router>
-      </div>
+    </div>
   );
 }
 
